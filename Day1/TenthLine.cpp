e668afc17fdbfcd4e7bacccf3ff4b39187bab2c0
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-05-06 12:52:07 +0400

    Add files via upload

---
+++ Day1/TenthLine.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+int main(){
+   
+    std::fstream f;
+    f.open("file.txt");
+    int numofline = 0;
+    int j = 0;
+    std::string line;
+    
+    while(!f.eof() &&  numofline != 10) {
+        
+        f >> line;
+        
+        if(f.peek() == '\n'){
+            ++numofline;
+        }
+        
+    }
+    std::cout<<line<<std::endl;
+    return 0;
+ 
+       
+}
