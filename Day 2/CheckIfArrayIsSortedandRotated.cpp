9307a182fdfb1e710d0fedfe0b1c915fb54cbb6a
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 18:48:53 +0400

    Update CheckIfArrayIsSortedandRotated.cpp

--- Day 2/CheckIfArrayIsSortedandRotated.cpp
+++ Day 2/CheckIfArrayIsSortedandRotated.cpp
@@ -5,21 +5,15 @@ public:
         int c=0;
         while(i<nums.size()){
            if(nums[i]>nums[(i+1)%nums.size()]){
-               
                ++c;
            } 
-          
            ++i;
-       }
-        
+        }
          if(c<2){
             return true;
          }
-        
         return false;
     }
-       
-                   
 };
       
     
