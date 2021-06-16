290dbcdaaa587b2d51956307ad18ebd38bfa4f31
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 18:54:41 +0400

    Update and rename Reverse Integer to ReverseInteger.cpp

--- Day 2/ReverseInteger.cpp
+++ Day 2/ReverseInteger.cpp
@@ -9,31 +9,23 @@ public:
             --j;
         }
     }
-    
     int reverse(int x) {
-       string s =to_string(x);
-       string tmp;
-    
+        string s =to_string(x);
+        string tmp;
         if(s[0]=='-'){
-            
             tmp="-";
             s.erase(0,1);
             reverseString(s);
             s.insert(0,tmp);
         }
-        
         else{
-            
             reverseString(s);
         }
-      
         try {
             return stoi(s);
         } 
-        
         catch(std::out_of_range e) { 
             return 0;
         }
-    
     }
 };
