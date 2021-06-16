f71518d9d4eda83b07d9045a3e3fe15b5d49986f
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 18:55:38 +0400

    Update and rename Rotate Array to RotateArray.cpp

--- Day 2/RotateArray.cpp
+++ Day 2/RotateArray.cpp
@@ -5,10 +5,8 @@ public:
         int size = nums.size();
         while (i < k) {
             int temp = nums[size-1];
-
             for (int j =size-1; j > 0; j--) {
                 nums[j] = nums[j - 1];
-
             }
             nums[0] = temp;
             i++;
