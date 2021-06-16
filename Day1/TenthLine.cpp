b98473cb0b98d82b75189f07a7ed4dfa0d5e0d6e
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 19:11:15 +0400

    Update TenthLine.cpp

--- Day1/TenthLine.cpp
+++ Day1/TenthLine.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 #include <fstream>
+
 int main(){
-   
     std::fstream f;
     f.open("file.txt");
     int numofline = 0;
     std::string line;
-    
     while(!f.eof() &&  numofline != 10) {
-        
         f >> line;
-        
         if(f.peek() == '\n'){
             ++numofline;
         }
-        
     }
     std::cout<<line<<std::endl;
     return 0;
- 
-       
 }
