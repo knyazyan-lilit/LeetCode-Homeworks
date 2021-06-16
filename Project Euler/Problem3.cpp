863e8f12a50f5ae3d791662d7e2fa16a4b13f12f
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 18:18:41 +0400

    Update Problem3.cpp

--- Project Euler/Problem3.cpp
+++ Project Euler/Problem3.cpp
@@ -10,16 +10,13 @@ bool isPrime(int i){
 }
 
 int main(){
-   
    int max=0;
    long int num = 600851475143;
    int i=2;
-   
    while(num != 1){
        if(num%i == 0){
            num = num/i;
            max = (i >= max) ? i : max;
-           
            i=2;
        }
        else{
@@ -30,5 +27,5 @@ int main(){
        }
    }
    std::cout<<max;
-
+   return 0;
 }
