6aa5391ef8d7586e05800b76417d02fbeb6b89bb
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-16 19:08:56 +0400

    Update and rename Sort Array By Parity to SortArrayByParity.cpp

--- Day1/SortArrayByParity.cpp
+++ Day1/SortArrayByParity.cpp
@@ -4,18 +4,15 @@ public:
         int i=0;
         int j=A.size()-1;
         while(i<j){
-        if(A[i]%2==0){
-            ++i;
-        }
+            if(A[i]%2==0){
+                ++i;
+            }
         else{
             std::swap(A[i],A[j]);
             --j;
-            
-            
         }
     }
         return A;
-       
     }
 };
 
