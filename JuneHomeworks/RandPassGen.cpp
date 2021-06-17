0fb9f771f7cffd72d6c377dd44a0c1f613dc0bed
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-17 09:55:28 +0400

    Update RandPassGen.cpp

--- JuneHomeworks/RandPassGen.cpp
+++ JuneHomeworks/RandPassGen.cpp
@@ -3,40 +3,34 @@
 
 int random_index_generator()
 {
-
     int r = rand() % 4;
     return r;
 }
 
 int alphabet_index_generator()
 {
-
     int r = rand() % 26;
     return r;
 }
 
 std::string PasswordGenerator()
 {
-
     int i = 0;
     std::string Uppercase;
     std::string Lowercase;
     std::string Numbers;
     std::string Symbols;
-
     while (i < 26)
     {
         Uppercase.push_back('A' + i);
         ++i;
     }
-
     i = 0;
     while (i < 26)
     {
         Lowercase.push_back('a' + i);
         ++i;
     }
-
     i = 0;
     while (i < 7)
     {
@@ -45,7 +39,6 @@ std::string PasswordGenerator()
     }
     i = 0;
     Symbols.push_back(')');
-
     while (i < 5)
     {
         Symbols.push_back('+' + i);
@@ -63,16 +56,13 @@ std::string PasswordGenerator()
         Symbols.push_back('{' + i);
         ++i;
     }
-
     std::string password;
     int length = 8 + (rand() % (24 - 8 + 1));
     int count_of_elems = 0;
     int count_of_uppercases = 0;
     int count_of_lowercases = 0;
     int count_of_symbols = 0;
-
     password.resize(length);
-
     while (count_of_elems < length)
     {
         int ind = random_index_generator();
@@ -80,17 +70,14 @@ std::string PasswordGenerator()
         {
             continue;
         }
-
         if (count_of_elems == length - 1 && count_of_symbols == 1)
         {
-
             int symbols = rand() % Symbols.size();
             password = password + Symbols[symbols];
             count_of_symbols++;
             count_of_elems++;
             break;
         }
-
         if (count_of_elems == length - 2 && count_of_symbols == 0)
         {
 
@@ -104,20 +91,17 @@ std::string PasswordGenerator()
             count_of_symbols++;
             count_of_elems++;
         }
-
         if (ind == 0)
         {
             if ((count_of_uppercases == 2) && (count_of_lowercases == 0 || count_of_lowercases == 1 || count_of_symbols == 0 || count_of_symbols == 1))
             {
                 continue;
             }
-
             int uppercase = alphabet_index_generator();
             password = password + Uppercase[uppercase];
             count_of_uppercases++;
             count_of_elems++;
         }
-
         if (ind == 1)
         {
             if ((count_of_lowercases == 2) && (count_of_uppercases == 0 || count_of_uppercases == 1 || count_of_symbols == 0 || count_of_symbols == 1))
@@ -132,7 +116,6 @@ std::string PasswordGenerator()
 
         if (ind == 2)
         {
-
             int number = rand() % 10;
             password = password + std::to_string(number);
             count_of_elems++;
@@ -140,7 +123,6 @@ std::string PasswordGenerator()
 
         if (ind == 3)
         {
-
             int symbols = rand() % Symbols.size();
             password = password + Symbols[symbols];
             count_of_symbols++;
