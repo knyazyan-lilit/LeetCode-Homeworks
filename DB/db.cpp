837f0609ff15fed84e3c133f457f3ec540995def
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-17 09:59:27 +0400

    Update db.cpp

--- DB/db.cpp
+++ DB/db.cpp
@@ -14,15 +14,12 @@ const std::string DROP = "drop";
 const std::string DELETE = "delete";
 std::set<std::string> files;
 
-
 void InsertInFile(std::vector<std::string> query){
-
     std::fstream f;
     std::string filename = query[1]+".txt";
     if(!query[1].empty() && files.find(filename) != files.end()){
         f.open(filename);
         int i = 0;
-        
         f.seekp(0,std::ios_base::end);
         while(query[i]!=query.back()){
             if(query[i] == ":"){
@@ -35,12 +32,10 @@ void InsertInFile(std::vector<std::string> query){
         }
             std::cout<<"Successfully inserted"<<std::endl;
         f << "-"<<'\n';
-        
     }
     else{
         std::cout<<"Query is wrong "<<std::endl;
     }
-        
 }
 
 void CreateFile(std::vector<std::string> query){
@@ -48,14 +43,11 @@ void CreateFile(std::vector<std::string> query){
     std::string filename = "./"+query[1]+".txt";
     std::string name = query[1] + ".txt";
     if(!query[1].empty() && files.find(filename) == files.end()){
-    
         if(files.find(query[1] + ".txt") != files.end()){
             std::cout<<"Already exists file with this name"<<std::endl;
             return;
         }
-
         else{
-            
             f.open(filename);
             files.insert(query[1]+".txt");
             std::cout<<"Successfully created"<<std::endl;
@@ -73,27 +65,22 @@ void DropFile(std::vector<std::string> query){
     if(!query[1].empty() && files.find(filename) != files.end()){
         char fn[filename.length()+1];
         strcpy(fn, filename.c_str());
-
         if( remove(fn) != 0 ){
             std::cout<< "Error deleting file"<<std::endl;
             return;
         }
-
         else{
             files.erase(filename);
             std::cout<<"File successfully deleted"<<std::endl;
             return;
         } 
     }
-
     else{
         std::cout<<"Query is wrong "<<std::endl;
     }
 }
 
-
 std::pair<std::string,std::string> FindFieldAndValue(std::vector<std::string> query){
-    
     int i = 0;
     while(query[i]!=query.back()){
         if(query[i] == ":"){
@@ -105,18 +92,15 @@ std::pair<std::string,std::string> FindFieldAndValue(std::vector<std::string> qu
 }
 
 std::vector<std::map<std::string,std::string>> ReadFileLineByLine(std::fstream& f,std::vector<std::map<std::string,std::string>>& v,int i,std::string &value,std::string &field){
-     
     while(true){
         field=value;
         f>>value;
         if(value == "-"){
             break;
         }
-
         if( value == ":"){
             f>>value;
             v[i].insert({field,value});
-            
         }
     }
    return v;
@@ -129,16 +113,12 @@ std::vector<std::map<std::string,std::string>> ParseEntries(std::string filename
     int i = 0;
     std::string value;
     std::string field;
-
     while(!f.eof()){
-        
         f>>value;
         ReadFileLineByLine(f,v,i,value,field);
         ++i;
-
     }
     return v;
-
 }
 
 void Print(std::map<std::string,std::string> entry){
@@ -166,7 +146,6 @@ void ReadFromFile(std::vector<std::string> query){
 }
 
 std::pair<std::string,std::string> FindFieldForUpdate(std::vector<std::string> query){
-    
     int i = 0;
     while(query[i]!=query.back()){
         if(query[i] == ":"){
@@ -195,38 +174,29 @@ std::pair<std::string,std::string> FindFieldAndValue1(std::vector<std::string> q
 
 
 void WriteInFile(std::vector<std::map<std::string,std::string>>entries,std::fstream& f ,int i){
-
     f<<"-"<<"\n";
     std::map<std::string, std::string>::iterator itr;
     for (itr = entries[i].begin(); itr != entries[i].end(); ++itr) {
       
         f<< itr->first<<" "<<":"<<" "<< itr->second<<"\n";
-        
     }
-    
 }
 
 
 void WriteInFileForDelete(std::vector<std::map<std::string,std::string>>entries,std::fstream& f ,int i){
-
-    
     std::map<std::string, std::string>::iterator itr;
     for (itr = entries[i].begin(); itr != entries[i].end(); ++itr) {
-       
         f<< itr->first<<" "<<":"<<" "<< itr->second<<"\n";
-        
     }
    f<<"-"<<"\n";
 }
 
 void Delete(std::vector<std::map<std::string,std::string>> &entries,std::pair< std::string,std::string> field_and_value,std::string filename ){
-    
     std::fstream f;
     f.open(filename, std::ios::out | std::ios::trunc);
     int i = 0;
     f<<"-"<<"\n";
     if(entries.empty()){
-        
         WriteInFileForDelete(entries, f,i);
     }
     else{
@@ -236,16 +206,13 @@ void Delete(std::vector<std::map<std::string,std::string>> &entries,std::pair< s
             ++i;
         }
     }
-     
 }
 
 void Update(std::vector<std::map<std::string,std::string>> &entries,std::pair< std::string,std::string> field_and_value,std::string filename ){
-    
     std::fstream f;
     f.open(filename);
     int i = 0;
     if(entries.empty()){
-        
         WriteInFile(entries, f,i);
     }
     else{
@@ -261,11 +228,9 @@ void Update(std::vector<std::map<std::string,std::string>> &entries,std::pair< s
 void UpdateField(std::vector<std::string> query){
     std::string filename = query[1] + ".txt";
     if(!query[1].empty() && files.find(filename) != files.end()){
-        
         std::pair< std::string,std::string> pair = FindFieldAndValue1(query);//returns conditional field and value
         std::pair< std::string,std::string> field_and_value = FindFieldForUpdate(query);//returns field and value that need to change
         std::vector<std::map<std::string,std::string>> entries = ParseEntries(filename);
-       
         for (auto & entry : entries) {
             if (entry.find(pair.first)->second == pair.second) {
                 entry[field_and_value.first] = field_and_value.second;
@@ -290,7 +255,6 @@ void DeleteInfo(std::vector<std::string> query){
                 Delete(entries,pair,filename);
                 std::cout<<"Deleted"<<std::endl;
             }
-            
         }
     }
     else{
@@ -300,35 +264,27 @@ void DeleteInfo(std::vector<std::string> query){
 
 
 void parse(std::vector<std::string>  query){
-      
     if(query[0] == CREATE){
         CreateFile(query);
     }
-
     if(query[0] == INSERT){
         InsertInFile(query);
     }
-
     if(query[0] == READ){
         ReadFromFile(query);
     }
-    
     if(query[0]==UPDATE){
         UpdateField(query);
     }
-
     if(query[0]==DROP){
        DropFile(query);
     }
-
     if(query[0]==DELETE){
         DeleteInfo(query);
     }
-
 }
 
 int main(){
-
     while(true){
         std::vector<std::string> query;
         while(std::cin){
@@ -339,10 +295,7 @@ int main(){
                 parse(query);
                 break;
             }
-               
         }
-      
     }
-
     return 0;
 }
