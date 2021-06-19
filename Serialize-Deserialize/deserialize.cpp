f89acfd32edd33f1f606b831e6f94c52e50ba994
knyazyan-lilit <67742030+knyazyan-lilit@users.noreply.github.com>
2021-06-19 17:30:33 +0400

    Add files via upload

---
+++ Serialize-Deserialize/deserialize.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+struct Person
+{
+private:
+    std::string name;
+    int age;
+    std::string id;
+public:
+    //setters
+    void set_name(std::string n)
+    {
+        name = n;
+    }
+    void set_age(int a)
+    {
+        age = a;
+    }
+    void set_id(std::string i)
+    {
+        id = i;
+    }
+    //getters
+    std::string get_name()
+    {
+        return name;
+    }
+    int get_age()
+    {
+        return age;
+    }
+    std::string get_id()
+    {
+        return id;
+    }
+};
+
+void save_in_vec(std::vector<Person> &v, std::fstream &file)
+{
+    std::string name;
+    std::string age;
+    std::string id;
+    int i = 0;
+    while (!file.eof())
+    {
+        Person p;
+        file >> name;
+        if (name == "---")
+        {
+            p.set_name(name);
+        }
+        else
+        {
+            std::getline(file, name, ':');
+            file >> name;
+            p.set_name(name);
+        }
+        file >> age;
+        if (age == "---")
+        {
+            p.set_age(0);
+        }
+        else
+        {
+            std::getline(file, age, ':');
+            file >> age;
+            p.set_age(atoi(age.c_str()));
+        }
+        file >> id;
+        if (id == "---")
+        {
+            p.set_id(id);
+        }
+        else
+        {
+            std::getline(file, id, ':');
+            file >> id;
+            p.set_id(id);
+        }
+        v.push_back(p);
+    }
+}
+
+int main()
+{
+    std::fstream file;
+    file.open("./f.txt");
+    std::vector<Person> v;
+    save_in_vec(v, file);
+    for (int i = 0; i < v.size() - 1; ++i)
+    {
+        std::cout << v[i].get_name() << " " << v[i].get_age() << " " << v[i].get_id() << std::endl;
+    }
+    return 0;
+}
---
+++ Serialize-Deserialize/serialize.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+struct Person
+{
+private:
+    std::string name;
+    int age;
+    std::string id;
+public:
+    //setters
+    void set_name(std::string n)
+    {
+        name = n;
+    }
+    void set_age(int a)
+    {
+        age = a;
+    }
+    void set_id(std::string i)
+    {
+        id = i;
+    }
+    //getters
+    std::string get_name()
+    {
+        return name;
+    }
+    int get_age()
+    {
+        return age;
+    }
+    std::string get_id()
+    {
+        return id;
+    }
+};
+
+void save_in_file(std::vector<Person> v, std::fstream &file)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
+        file << "name : " << v[i].get_name() << '\n';
+        file << "age : " << v[i].get_age() << '\n';
+        file << "id : " << v[i].get_id() << '\n';
+    }
+}
+
+int main()
+{
+    std::string name;
+    int age;
+    std::string id;
+    std::vector<Person> v;
+    int count_of_persons;
+    std::cin >> count_of_persons;
+    int i = 0;
+    while (i != count_of_persons && std::cin >> name && std::cin >> age && std::cin >> id)
+    {
+        Person p;
+        p.set_name(name);
+        p.set_age(age);
+        p.set_id(id);
+        v.push_back(p);
+        ++i;
+    }
+    std::fstream file;
+    file.open("./f.txt");
+    save_in_file(v, file);
+    file.clear();
+    return 0;
+}
